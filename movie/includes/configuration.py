9baebdb2b5578a900ca4a6e87675466f0320b6c6
meganxlin <110056811+meganxlin@users.noreply.github.com>
2022-08-22 02:09:25 -0500

    Update configuration.py

--- movie/includes/configuration.py
+++ movie/includes/configuration.py
@@ -8,16 +8,3 @@ bronzePath = myPath + "bronze/"
 silverPath = myPath + "silver/"
 silverQuarantinePath = myPath + "silverQuarantine/"
 goldPath = myPath + "gold/"
-
-# COMMAND ----------
-
-spark.sql(f"CREATE DATABASE IF NOT EXISTS moviebricks_{username}")
-spark.sql(f"USE moviebricks_{username}")
-
-# COMMAND ----------
-
-# MAGIC %run ./utilities
-
-# COMMAND ----------
-
-
